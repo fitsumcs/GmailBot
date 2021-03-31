917ad31ce79b07ba3fc7bbcd46f367bdb3c35ba8
Fitsum A <fitsum@gmail.com>
2021-03-31 22:46:33 +0300

    adding the code to a function get_voice

--- mailBot.py
+++ mailBot.py
@@ -2,15 +2,18 @@ import speech_recognition as sp
 
 listner = sp.Recognizer()
 
-try:
-    with sp.Microphone() as source:
-        print("I am listining..")
-        voice = listner.listen(source)
-        text = listner.recognize_google(voice)
-        print(text)
+def get_voice():
+    try:
+        with sp.Microphone() as source:
+            print("I am listining..")
+            voice = listner.listen(source)
+            text = listner.recognize_google(voice)
+            print(text)
 
-except:
-    print("Some Error")
-    pass
+    except:
+        print("Some Error")
+        pass
+
+get_voice()
 
 
