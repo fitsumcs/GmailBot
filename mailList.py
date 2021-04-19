b0e0fdbfbba845f4ad1cce44c9df3ed064f24c92
Fitsum A <fitsumcs@gmail.com>
2021-04-19 16:38:04 +0400

    Adding mail list for exctracting mail

---
+++ mailList.py
@@ -0,0 +1,11 @@
+email_list = {
+    'dude': 'COOL_DUDE_EMAIL',
+    'bts': 'diamond@bts.com',
+    'pink': 'jennie@blackpink.com',
+    'lisa': 'lisa@blackpink.com',
+    'irene': 'irene@redvelvet.com'
+}
+
+
+def getMail(name):
+    return email_list[name]
