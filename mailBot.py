a6a2cd087742d014e6d55c53b9d39b74016c92ab
Fitsum A <fitsumcs@gmail.com>
2021-04-19 12:15:58 +0400

    adding few files for audio testing

--- mailBot.py
+++ mailBot.py
@@ -1,19 +1,32 @@
 import speech_recognition as sp
 
-listner = sp.Recognizer()
+listener = sp.Recognizer()
+
+# for index, name in enumerate(sp.Microphone.list_microphone_names()):
+#     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
+# mic =sp.AudioFile('./pzm12.wav')
+mic = sp.Microphone()
 
 def get_voice():
-    try:
-        with sp.Microphone() as source:
+    with mic as source:
             print("I am listining..")
-            voice = listner.listen(source)
-            text = listner.recognize_google(voice)
+            audio = listener.listen(source)
+    try:
+            text = listener.recognize_google(audio)
             print(text)
+            # return text.lower()
 
-    except:
-        print("Some Error")
+    except Exception as e :
+        print(e)
         pass
 
+# Get Email Information to be send 
+# def getEmail_Info():
+    
+
+
 get_voice()
+
 
 
